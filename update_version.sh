db6ee49c938a516f6a3ba7984dae696d976414e7
Jumaru <spiralout.eu@gmail.com>
2023-10-28 23:05:14 +0300

    fix(ci): :bug: Update the version in Cargo.lock with update_version script

--- update_version.sh
+++ update_version.sh
@@ -5,15 +5,16 @@
 
 NEW_VERSION=$1
 CARGO_TOML="Cargo.toml"
+CARGO_LOCK="Cargo.lock"
 
 # Update the version in Cargo.toml
 sed -i.bak -e "s/^version = .*/version = \"$NEW_VERSION\"/" $CARGO_TOML
+# Update the version in Cargo.lock
+sed -i.bak "/name = \"zk_whitelist\"/,/version =/s/version = \"[^\"]*\"/version = \"$NEW_VERSION\"/" $CARGO_LOCK
 
 # Remove the backup file created by sed
 rm $CARGO_TOML.bak
-
-# Update Cargo.lock file
-cargo update
+rm $CARGO_LOCK.bak
 
 # Build the project with the updated version number
 cargo build --release
