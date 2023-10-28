ae42c866a6b7b7d974af4ef59009fd201de2748d
Jumaru <spiralout.eu@gmail.com>
2023-10-28 15:53:08 +0300

    feat(cli): :sparkles: Add --version and --v commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.contains(&"--version".to_string()) || args.contains(&"--v".to_string()) {
+        println!("Zero Knowledge Whitelist Tool, version {}", env!("CARGO_PKG_VERSION"));
+    }
 }
---
+++ tests/version_command_tests.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod tests {
+    // use super::*;
+    use assert_cmd::Command;
+
+    #[test]
+    fn test_version_command() {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.arg("--version")
+            .assert()
+            .success()
+            .stdout(predicates::str::contains(env!("CARGO_PKG_VERSION")));
+
+        cmd.arg("--v")
+            .assert()
+            .success()
+            .stdout(predicates::str::contains(env!("CARGO_PKG_VERSION")));
+    }
+}
