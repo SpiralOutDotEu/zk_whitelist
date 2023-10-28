4eee73057a1aa0522c3289c97bd2fe79544dbf94
Jumaru <spiralout.eu@gmail.com>
2023-10-28 17:51:01 +0300

    fix(ci): :bug: change order of release actions

--- release.config.js
+++ release.config.js
@@ -3,14 +3,14 @@ module.exports = {
     plugins: [
       "@semantic-release/commit-analyzer",
       "@semantic-release/release-notes-generator",
+      ["@semantic-release/exec", {
+        "prepareCmd": "./update_version.sh ${nextRelease.version}"
+      }],
       ["@semantic-release/changelog", {
         "changelogFile": "CHANGELOG.md",
       }],
       ["@semantic-release/github", {
         "assets": "target/release/zk_whitelist"
-      }],
-      ["@semantic-release/exec", {
-        "prepareCmd": "./update_version.sh ${nextRelease.version}"
       }],
       ["@semantic-release/git", {
         "assets": ["CHANGELOG.md", "Cargo.toml"],
