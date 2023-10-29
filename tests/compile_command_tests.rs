ab3aa7ef01fa4459f44009930ac40132c22d7675
Jumaru <spiralout.eu@gmail.com>
2023-10-29 17:46:44 +0200

    refactor(cli): :recycle: Refactor test_compile_command for improved readability and maintainability

--- tests/compile_command_tests.rs
+++ tests/compile_command_tests.rs
@@ -1,43 +1,46 @@
 #[cfg(test)]
 mod tests {
     use assert_cmd::Command;
+    use std::error::Error;
     use std::fs;
-    use std::path::Path;
+    use std::path::{Path, PathBuf};
+    use tempfile::tempdir;
 
     #[test]
     fn test_compile_command() {
         // Arrange
-        let current_dir = std::env::current_dir().unwrap();
-        let r1cs_path = Path::new(&current_dir).join("circuit.r1cs");
-        let sym_path = Path::new(&current_dir).join("circuit.sym");
-        let wasm_dir_path = Path::new(&current_dir).join("circuit_js");
-        let wasm_path = Path::new(&current_dir).join("circuit_js/circuit.wasm");
+        let temp_dir = tempdir().unwrap();
+        let current_dir = temp_dir.path().to_path_buf();
+        create_circuit_file(&current_dir).unwrap();
 
-        // Act: Execute the circuit command to create circuit.circom
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg("circuit")
-            .assert()
-            .success();
-
-        // Reset the command object
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-
-        // Act: Execute the compile command
-        cmd.arg("compile")
-            .assert()
-            .success();
+        // Act
+        execute_compile_command(&current_dir);
 
         // Assert
+        assert_compiled_files_exist(&current_dir);
+
+    }
+
+    fn create_circuit_file(dir: &Path) -> Result<PathBuf, Box<dyn Error>> {
+        let circuit_path = dir.join("circuit.circom");
+        fs::write(&circuit_path, include_bytes!("../templates/circuit.circom"))?;
+        Ok(circuit_path)
+    }
+
+    fn execute_compile_command(dir: &Path) {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.current_dir(dir)
+            .arg("compile")
+            .assert()
+            .success();
+    }
+
+    fn assert_compiled_files_exist(dir: &Path) {
+        let r1cs_path = dir.join("circuit.r1cs");
+        let sym_path = dir.join("circuit.sym");
+        let wasm_dir_path = dir.join("circuit_js");
         assert!(r1cs_path.exists());
         assert!(sym_path.exists());
-        assert!(wasm_path.exists());
         assert!(wasm_dir_path.exists());
-
-        // Clean up: Remove the created files
-        fs::remove_file(Path::new(&current_dir).join("circuit.circom")).expect("Failed to remove circuit.circom");
-        fs::remove_file(r1cs_path).expect("Failed to remove circuit.r1cs");
-        fs::remove_file(sym_path).expect("Failed to remove circuit.sym");
-        fs::remove_file(wasm_path).expect("Failed to remove circuit.wasm");
-        fs::remove_dir_all(wasm_dir_path).expect("Failed to remove circuit_js directory");
     }
 }
