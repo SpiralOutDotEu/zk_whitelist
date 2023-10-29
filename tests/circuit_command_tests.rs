5678fea4cc11b7a792c47b5fbc3435f827c01dd0
Jumaru <spiralout.eu@gmail.com>
2023-10-30 00:51:18 +0200

    style(cli): :technologist: Apply fmt

--- tests/circuit_command_tests.rs
+++ tests/circuit_command_tests.rs
@@ -1,11 +1,9 @@
 #[cfg(test)]
-/// Module for testing the circuit command functionality.
 mod tests {
     use assert_cmd::Command;
     use std::fs;
     use std::path::Path;
 
-    /// Test to ensure the circuit command creates the circuit file with correct content
     #[test]
     fn test_circuit_command() {
         // Arrange
@@ -13,10 +11,10 @@ mod tests {
         let circuit_path = current_dir.join("circuit.circom");
         let expected_content = include_bytes!("../templates/circuit.circom");
 
-        // Act: Execute the circuit command
+        // Act
         execute_circuit_command();
 
-        // Assert: Verify the content of the created file
+        // Assert
         let actual_content = fs::read(&circuit_path).unwrap();
         assert_files_match(expected_content, &actual_content, &circuit_path);
 
@@ -24,13 +22,11 @@ mod tests {
         fs::remove_file(circuit_path).unwrap();
     }
 
-    // Function to execute the circuit command
     fn execute_circuit_command() {
         let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
         cmd.arg("circuit").assert().success();
     }
 
-    // Function to compare the content of the files
     fn assert_files_match(expected_content: &[u8], actual_content: &[u8], file_path: &Path) {
         assert!(file_path.exists());
         assert_eq!(expected_content, actual_content);
--- tests/compile_command_tests.rs
+++ tests/compile_command_tests.rs
@@ -1,39 +1,35 @@
 #[cfg(test)]
-/// Module for testing the compile command functionality.
 mod tests {
     use assert_cmd::Command;
     use std::fs;
     use std::path::Path;
 
-    /// Test to ensure the compile command compiles the circuit file and produces the expected files
     #[test]
     fn test_compile_command() {
-        // Arrange: Create the circuit file
+        // Arrange
         let current_dir = std::env::current_dir().unwrap();
         create_circuit_file(&current_dir);
 
-        // Act: Execute the compile command
+        // Act
         execute_compile_command(&current_dir);
 
-        // Assert: Verify the compiled files exist
+        // Assert
         assert_compiled_files_exist(&current_dir);
 
-        // Clean up: Remove the created and compiled files
+        // Clean up
         clean_up_files(&current_dir);
     }
 
-    // Function to create a circuit file in the specified directory
     fn create_circuit_file(dir: &Path) {
         let circuit_path = dir.join("circuit.circom");
         fs::write(&circuit_path, include_bytes!("../templates/circuit.circom")).unwrap();
     }
 
-    // Function to execute the compile command
     fn execute_compile_command(dir: &Path) {
         let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
         cmd.current_dir(dir).arg("compile").assert().success();
     }
-    // Function to assert that the compiled files exist
+
     fn assert_compiled_files_exist(dir: &Path) {
         let r1cs_path = dir.join("circuit.r1cs");
         let sym_path = dir.join("circuit.sym");
@@ -43,7 +39,6 @@ mod tests {
         assert!(wasm_dir_path.exists());
     }
 
-    // Function to clean up the created and compiled files
     fn clean_up_files(dir: &Path) {
         fs::remove_file(dir.join("circuit.circom")).unwrap();
         fs::remove_file(dir.join("circuit.r1cs")).unwrap();
