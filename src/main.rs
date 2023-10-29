943931b8eaf1e30576a0dfb354524d637ac6aaa1
Jumaru <spiralout.eu@gmail.com>
2023-10-29 12:32:09 +0200

    feat(cli): :sparkles: Add `circuit` command to create `circuit.circom` on user's folder

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.contains(&"--version".to_string()) || args.contains(&"--v".to_string()) {
-        println!("Zero Knowledge Whitelist Tool, version {}", env!("CARGO_PKG_VERSION"));
+        println!(
+            "Zero Knowledge Whitelist Tool, version {}",
+            env!("CARGO_PKG_VERSION")
+        );
+    } else if args.contains(&"circuit".to_string()) {
+        let current_dir = env::current_dir()?;
+        let circuit_path = Path::new(&current_dir).join("circuit.circom");
+        let mut file = File::create(circuit_path)?;
+        file.write_all(include_bytes!("../templates/circuit.circom"))?;
     }
+
+    Ok(())
 }
