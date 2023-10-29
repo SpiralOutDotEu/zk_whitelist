a1d6126f6f4f2a230215dedef894a475176c59b5
Jumaru <spiralout.eu@gmail.com>
2023-10-29 12:44:03 +0200

    refactor(cli): :recycle: Improve readability by breaking down logic into functions

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,25 @@ use std::path::Path;
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.contains(&"--version".to_string()) || args.contains(&"--v".to_string()) {
-        println!(
-            "Zero Knowledge Whitelist Tool, version {}",
-            env!("CARGO_PKG_VERSION")
-        );
+        display_version();
     } else if args.contains(&"circuit".to_string()) {
-        let current_dir = env::current_dir()?;
-        let circuit_path = Path::new(&current_dir).join("circuit.circom");
-        let mut file = File::create(circuit_path)?;
-        file.write_all(include_bytes!("../templates/circuit.circom"))?;
+        copy_circuit_file()?;
     }
 
+    Ok(())
+}
+
+fn display_version() {
+    println!(
+        "Zero Knowledge Whitelist Tool, version {}",
+        env!("CARGO_PKG_VERSION")
+    );
+}
+
+fn copy_circuit_file() -> io::Result<()> {
+    let current_dir = env::current_dir()?;
+    let circuit_path = Path::new(&current_dir).join("circuit.circom");
+    let mut file = File::create(circuit_path)?;
+    file.write_all(include_bytes!("../templates/circuit.circom"))?;
     Ok(())
 }
