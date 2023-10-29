79afca95d21090bd01cb42ad46d4de9303bb595f
Jumaru <spiralout.eu@gmail.com>
2023-10-30 00:29:27 +0200

    feat(cli): :sparkles: Migrate to clap for argument parsing
    Migrate the CLI argument parsing to use the clap library, enhancing the CLI's usability and maintainability. This change introduces a structured way of handling command-line arguments and subcommands, making it easier to extend the CLI with new features in the future.  Additionally, this migration facilitates better error handling and provides users with more informative help messages.
    
     - Implement `Cli` struct and `SubCommand` enum for structured argument parsing
     - Update `main` function to use clap for argument handling
     - Replace manual argument parsing with clap's automatic parsing
     - Improve error messages and help output by leveraging clap's built-in functionalities

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
+use clap::{Parser, Subcommand};
 use std::env;
 use std::fs::File;
 use std::io::{self, Write};
 use std::path::Path;
 use std::process::Command;
 
+/// Represents the command line interface for the Zero Knowledge Whitelist Tool.
+/// Deriving `Parser` from clap allows for automatic parsing of command line arguments.
+#[derive(Parser)]
+#[clap(
+    name = "Zero Knowledge Whitelist Tool",
+    version = env!("CARGO_PKG_VERSION"),
+    author = "Nikos Koumbakis <n.koumbakis@gmail.com>",
+    about = "This tool orchestrates the management of an address whitelist using Zero-Knowledge (ZK) proofs.\nSimply input the addresses, and it will generate the corresponding Solidity code.\nIt streamlines the process of maintaining a secure and efficient whitelist for your decentralized application."
+)]
+struct Cli {
+    /// The subcommand to be executed, parsed from the command line arguments.
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+/// Enumerates the available subcommands.
+/// Deriving `Subcommand` from clap provides automatic subcommand handling.
+#[derive(Subcommand)]
+enum SubCommand {
+    /// The `circuit` subcommand copies a circuit file template to the current directory.
+    Circuit,
+    /// The `compile` subcommand compiles the circuit file.
+    Compile,
+}
+/// The entry point of the application.
+/// Parses command line arguments and executes the corresponding subcommand.
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.contains(&"--version".to_string()) || args.contains(&"--v".to_string()) {
-        display_version();
-    } else if args.contains(&"circuit".to_string()) {
-        copy_circuit_file()?;
-    } else if args.contains(&"compile".to_string()) {
-        compile_circuit()?;
+    let args = Cli::parse();
+
+    match args.subcmd {
+        SubCommand::Circuit => copy_circuit_file()?,
+        SubCommand::Compile => compile_circuit()?,
     }
 
     Ok(())
 }
 
-fn display_version() {
-    println!(
-        "Zero Knowledge Whitelist Tool, version {}",
-        env!("CARGO_PKG_VERSION")
-    );
-}
-
+/// Copies a circuit file template to the current directory.
+/// This function is called when the `circuit` subcommand is used.
 fn copy_circuit_file() -> io::Result<()> {
     let current_dir = env::current_dir()?;
     let circuit_path = Path::new(&current_dir).join("circuit.circom");
@@ -32,6 +52,8 @@ fn copy_circuit_file() -> io::Result<()> {
     Ok(())
 }
 
+/// Compiles the circuit file using the circom compiler.
+/// This function is called when the `compile` subcommand is used.
 fn compile_circuit() -> io::Result<()> {
     let current_dir = env::current_dir()?;
     eprintln!("Working directory: {:?}", current_dir);
@@ -40,14 +62,13 @@ fn compile_circuit() -> io::Result<()> {
         .arg("circuit.circom")
         .args(&["--r1cs", "--sym", "--wasm"])
         .output()?;
-    
+
     eprintln!("circom stdout: {}", String::from_utf8_lossy(&output.stdout));
     eprintln!("circom stderr: {}", String::from_utf8_lossy(&output.stderr));
 
-    
     if !output.status.success() {
         return Err(io::Error::new(io::ErrorKind::Other, "Compilation failed"));
     }
-    
+
     Ok(())
 }
