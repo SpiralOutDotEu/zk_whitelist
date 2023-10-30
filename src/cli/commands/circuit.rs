52af97ef22deb51eb38af4d2718cff3a1e79545e
Jumaru <spiralout.eu@gmail.com>
2023-10-30 12:28:09 +0200

    refactor(cli): :recycle: Simplify test

--- src/cli/commands/circuit.rs
+++ src/cli/commands/circuit.rs
@@ -3,21 +3,9 @@ use std::fs::File;
 use std::io::{self, Write};
 use std::path::Path;
 
-pub trait FileSystem {
-    fn create(&self, path: &Path) -> std::io::Result<Box<dyn std::io::Write>>;
-}
-
-pub struct RealFileSystem;
-
-impl FileSystem for RealFileSystem {
-    fn create(&self, path: &Path) -> std::io::Result<Box<dyn std::io::Write>> {
-        File::create(path).map(|file| Box::new(file) as Box<dyn std::io::Write>)
-    }
-}
-
 /// Copies a circuit file template to the current directory.
 /// This function is called when the `circuit` subcommand is used.
-fn copy_circuit_file(fs: &dyn FileSystem) -> io::Result<()> {
+fn copy_circuit_file() -> io::Result<()> {
     let current_dir = env::current_dir()?;
     let circuit_path = Path::new(&current_dir).join("circuit.circom");
     let mut file = File::create(circuit_path)?;
@@ -26,6 +14,27 @@ fn copy_circuit_file(fs: &dyn FileSystem) -> io::Result<()> {
 }
 
 /// Handles CLI sub command
-pub fn handle_circuit_subcommand(fs: &dyn FileSystem) -> std::io::Result<()> {
-    copy_circuit_file(fs)
+pub fn handle_circuit_subcommand() -> std::io::Result<()> {
+    copy_circuit_file()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::path::Path;
+
+    #[test]
+    fn test_handle_circuit_subcommand() -> std::io::Result<()> {
+        handle_circuit_subcommand()?;
+
+        let current_dir = std::env::current_dir()?;
+        let circuit_path = Path::new(&current_dir).join("circuit.circom");
+        assert!(circuit_path.exists());
+
+        // Clean up
+        fs::remove_file(circuit_path)?;
+
+        Ok(())
+    }
 }
