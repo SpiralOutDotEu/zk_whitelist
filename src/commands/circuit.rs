e010ba01d7aa7ddc42cd0a12c6c00bfead5f3502
Jumaru <spiralout.eu@gmail.com>
2023-10-30 11:32:34 +0200

    feat(cli): :sparkles: Add circuit module with copy_circuit_file function

---
+++ src/commands/circuit.rs
@@ -0,0 +1,14 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, Write};
+use std::path::Path;
+
+/// Copies a circuit file template to the current directory.
+/// This function is called when the `circuit` subcommand is used.
+pub fn copy_circuit_file() -> io::Result<()> {
+    let current_dir = env::current_dir()?;
+    let circuit_path = Path::new(&current_dir).join("circuit.circom");
+    let mut file = File::create(circuit_path)?;
+    file.write_all(include_bytes!("../templates/circuit.circom"))?;
+    Ok(())
+}
