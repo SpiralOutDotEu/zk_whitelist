d3f0cbfb4b76608aece920dbdb6a7874a2d820f8
Jumaru <spiralout.eu@gmail.com>
2023-10-30 11:35:18 +0200

    feat(cli): :sparkles: Add compile module with compile_circuit function

---
+++ src/commands/compile.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::io;
+use std::process::Command;
+
+/// Compiles the circuit file using the circom compiler.
+/// This function is called when the `compile` subcommand is used.
+pub fn compile_circuit() -> io::Result<()> {
+    let current_dir = env::current_dir()?;
+    eprintln!("Working directory: {:?}", current_dir);
+    let output = Command::new("circom")
+        .current_dir(&current_dir)
+        .arg("circuit.circom")
+        .args(&["--r1cs", "--sym", "--wasm"])
+        .output()?;
+
+    eprintln!("circom stdout: {}", String::from_utf8_lossy(&output.stdout));
+    eprintln!("circom stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    if !output.status.success() {
+        return Err(io::Error::new(io::ErrorKind::Other, "Compilation failed"));
+    }
+
+    Ok(())
+}
