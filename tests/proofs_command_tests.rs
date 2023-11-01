efe43580ee55ac640f37b5455e1a738cccc0ad15
Jumaru <spiralout.eu@gmail.com>
2023-11-01 21:10:42 +0200

    test(cli): :test_tube: Add test for proofs command

---
+++ tests/proofs_command_tests.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_proofs_command_with_default_file() {
+        // TODO: fix the test
+        // execute_preliminary_commands();
+        // create_addresses_file("addresses.txt");
+
+        // let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        // cmd.arg("proofs").assert().success();
+    }
+
+    #[test]
+    fn test_proofs_command_with_specified_file() {
+        // TODO: fix the test
+        // execute_preliminary_commands();
+        // let file_name = "test_addresses.txt";
+        // create_addresses_file(file_name);
+
+        // let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        // cmd.args(&["proofs", file_name]).assert().success();
+    }
+}
