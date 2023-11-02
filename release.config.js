16c84ea22760cac1d84e4cffd68d69a35d5d2eab
Jumaru <spiralout.eu@gmail.com>
2023-11-02 10:37:52 +0200

    ci(ci): :construction_worker: Build binary releases for linux, mac and windows

--- release.config.js
+++ release.config.js
@@ -1,20 +1,24 @@
 module.exports = {
-    branches: ["master"],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      ["@semantic-release/exec", {
-        "prepareCmd": "./update_version.sh ${nextRelease.version}"
-      }],
-      ["@semantic-release/changelog", {
-        "changelogFile": "CHANGELOG.md",
-      }],
-      ["@semantic-release/github", {
-        "assets": "target/release/zk_whitelist"
-      }],
-      ["@semantic-release/git", {
-        "assets": ["CHANGELOG.md", "Cargo.toml", "Cargo.lock"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }],
-    ]
-  }
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/exec", {
+      "prepareCmd": "./update_version.sh ${nextRelease.version}"
+    }],
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md",
+    }],
+    ["@semantic-release/github", {
+      "assets": [
+        {"path": "target/release/zk_whitelist", "label": "Linux binary"},
+        {"path": "target/release/zk_whitelist.exe", "label": "Windows binary"},
+        {"path": "target/release/zk_whitelist.dmg", "label": "macOS binary"}
+      ]
+    }],
+    ["@semantic-release/git", {
+      "assets": ["CHANGELOG.md", "Cargo.toml", "Cargo.lock"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+  ]
+}
