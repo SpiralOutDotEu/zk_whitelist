75da39d71fbb63d09ea928219cf6d9bb0992b151
Jumaru <spiralout.eu@gmail.com>
2023-11-03 11:15:48 +0200

    ci(ci): :sparkles: Add multi-os builds for win & mac

--- update_version.sh
+++ update_version.sh
@@ -18,3 +18,19 @@ rm $CARGO_LOCK.bak
 
 # Build the project with the updated version number
 cargo build --release
+
+# Prepare for multi-os build
+sudo apt-get install podman
+cargo install cross
+
+# Build for Windows
+rustup target add x86_64-pc-windows-gnu
+
+# build for Mac
+curl -L https://github.com/roblabla/MacOSX-SDKs/releases/download/13.3/MacOSX13.3.sdk.tar.xz | tar xJ
+export SDKROOT=$(pwd)/MacOSX13.3.sdk/
+export PATH=$PATH:~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/
+export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=rust-lld
+rustup target add x86_64-apple-darwin
+cargo build --release --target x86_64-apple-darwin
+
